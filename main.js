75ed5c764f7c5ba3e39fece52d64b53a9ada6b27
MatteoBonny <matteobonanno6@gmail.com>
2023-10-05 17:59:45 +0200

    reload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+var cardWork = document.getElementById("work1");
+
+cardWork.addEventListener('click', function() {
+    window.location.href = "prova.html";
+    
+});
+
+
+
+function aggiornaOra() {
+    let elementoOra = document.getElementById('ora');
+    let dataAttuale = new Date();
+    let ora = dataAttuale.getHours();
+    let minuti = dataAttuale.getMinutes();
+    minuti = minuti < 10 ? '0' + minuti : minuti;
+    let oraFormattata = "Bonny IT " + ora + ':' + minuti;
+    elementoOra.textContent = oraFormattata;
+}
+aggiornaOra();
+setInterval(aggiornaOra, 60000);
+
+
+
+
+
+
