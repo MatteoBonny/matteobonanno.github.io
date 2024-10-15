1ed207de6889439b9dc2efa6e46585db24c2cf0d
Matteo Bonanno <m.bonanno@ied.edu>
2024-10-15 18:23:33 +0200

    build

--- src/layouts/GridLayout/GridLayout.jsx
+++
@@ -1,8 +0,0 @@
-export default function GridLayout({children}) {
-
-    return(
-       <div className="w-full grid grid-cols-12 md: gap-x-4 lg:gap-x-8">
-            {children}
-       </div>
-    );
-}
